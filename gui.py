77361db743ca2ec8f7d24a668fcfa8205cb77157
Marcus Corbin <mgc1g18@soton.ac.uk>
2020-12-23 11:34:31 +0000

    inital commit

---
+++ gui.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Youtube to MP3")
+
+frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
+frm_form.pack()
+
+label_url = tk.Label(master=frm_form, text="Enter URL:")
+entry_url = tk.Entry(master=frm_form, width=50)
+
+label_url.grid(row=0, column=0)
+entry_url.grid(row=0, column=1)
+
+frm_buttons = tk.Frame()
+frm_buttons.pack(fill=tk.X, ipadx=5, ipady=5)
+
+btn_convert = tk.Button(master=frm_buttons, text="Convert!")
+btn_convert.pack(side=tk.RIGHT, padx=10, ipadx=10)
+
+window.mainloop()
